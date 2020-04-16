534a9613c769efd261f25f7dd617c9a2ab094f98
George Whiting <gwhi94@gmail.com>
2020-04-16 19:34:51 +0100

    spys

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,17 +1,35 @@
 import { CalculatorService } from '../services/calculator.service';
 import { LoggerService } from './logger.service';
 
+
 describe('CalculatorService', () => {
+
+
+    let calculator:CalculatorService,
+        loggerSpy: any;
+        
+
+    beforeEach(() => {
+        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);      
+        calculator = new CalculatorService(loggerSpy);
+
+    });
+
+
     it('should add two numbers', () => {
-        const calculator = new CalculatorService(new LoggerService());
-
+        
+        
         const result = calculator.add(2,2);
-
         expect(result).toBe(4);
+
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
     });
 
     it('should subtract two numbers', () => {
-        pending();
+       
+        const result = calculator.subtract(4,2);
+        expect(result).toBe(2, "Unexpected subtraction result");
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     })
 })
