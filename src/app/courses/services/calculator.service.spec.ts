254f0e61928b719f695b625cbefc84eb00c7ea5f
George Whiting <gwhi94@gmail.com>
2020-04-16 21:00:56 +0100

    courses test

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,27 +1,32 @@
 import { CalculatorService } from '../services/calculator.service';
 import { LoggerService } from './logger.service';
+import { TestBed } from '@angular/core/testing';
 
 
 describe('CalculatorService', () => {
 
-
     let calculator:CalculatorService,
         loggerSpy: any;
         
-
     beforeEach(() => {
-        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);      
-        calculator = new CalculatorService(loggerSpy);
+        
+        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);  
+        
+        TestBed.configureTestingModule({
+            providers:[
+                CalculatorService,
+                { provide: LoggerService, useValue:loggerSpy }
+            ]
+        })
 
+        calculator = TestBed.get(CalculatorService);
     });
 
 
     it('should add two numbers', () => {
-        
-        
+               
         const result = calculator.add(2,2);
         expect(result).toBe(4);
-
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
     });
